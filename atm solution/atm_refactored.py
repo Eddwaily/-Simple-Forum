dad8337f0a691a3a29123bf2e043b19bdeef06cb
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-14 16:31:12 +0200

    slight changes

--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -1,18 +1,19 @@
 
-def withDraw(money,request):
-    paperValues=[100,50,10,5,1]
+def withDraw( money, request):
+    paperValues= [ 100, 50, 10, 5, 1]
     assert request>0, "Invalid input"
     assert request<=money, "Balance Not Enough !"
     remaining=money-request
-    print "Current Balance: "+str(money)
-    print "Delivering "+str(request)+"$ to client:"
+
+    print "Current Balance: "+ str(money)
+    print "Delivering "+ str(request)+ "$ to client:"
     for x in paperValues:
-        while (request>=x):
-            print "give"+str(x)
+        while request >= x:
+            print "give"+ str(x)
             request-=x
     return remaining
     print "**** Done ! ****"
 
 #test any value here
-balance=1000
+balance= 1000
 balance= withDraw(balance,250)
