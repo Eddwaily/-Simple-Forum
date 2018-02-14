5fb5a87651de3ce80c1a6f6594bf509acf98dfcc
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-14 16:07:55 +0200

    Update atm.py

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,22 +1,16 @@
 
-def withDraw(money,request):
+def withDraw(request):
+    money=500
     paperValues=[100,50,10,5,1]
     assert request>0, "Invalid input"
     assert request<=money, "Balance Not Enough !"
-    remaining=money-request
-    
     print "*************"
     print "Delivering "+str(request)+"$ to client:"
     for x in paperValues:
         while (request>=x):
             print "give"+str(x)
             request-=x
-    return remaining
-    print "Done !"
+    print "**** Done ! ****"
 
 #test any value here
-balance=500
-balance=withDraw(balance,200)
-balance=withDraw(balance,119)
-balance=withDraw(balance,181)
-balance=withDraw(balance,1)
+withDraw(250)
