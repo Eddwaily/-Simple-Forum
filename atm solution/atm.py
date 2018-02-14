f84a98e04cb58ea437cca2e5ab075023969d943f
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-14 21:07:13 +0200

    improvements
    some improvements for spacing and visual code stuff

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,16 +1,20 @@
 
 def withdraw(request):
-    money= 500
-    paperValues= [ 100, 50, 10, 5, 1]
-    assert request>0, "Invalid input"
-    assert request<=money, "Balance Not Enough !"
-    print "*************"
-    print "Delivering "+ str(request)+ "$ to client:"
-    for x in paperValues :
-        while request>=x :
-            print "give"+ str(x)
-            request-= x
-    print "**** Done ! ****"
-    
+    money = 500
+    paperValues = [ 100, 50, 10, 5, 1]
+    if request <= 0 :
+        print "Invalid input !" ; return 0
+    elif request > money:
+        print "Balance Not Enough !" ; return 0
+    else:
+        print "*************"
+        print "Delivering "+ str(request)+ "$ to client:"
+        for x in paperValues :
+            while request >= x :
+                print "give" + str(x)
+                request -= x
+        print "**** Done ! ****"
+        return 1
+
 #test any value here
 withdraw(250)
--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -1,19 +1,24 @@
 
 def withdraw( money, request):
     paperValues= [ 100, 50, 10, 5, 1]
-    assert request>0, "Invalid input"
-    assert request<=money, "Balance Not Enough !"
-    remaining=money-request
+
+    if request <= 0 :
+        print "Invalid input !" ; return money
+    if request > money :
+        print "Balance Not Enough !" ; return money
+
+    remaining= money - request
 
     print "Current Balance: "+ str(money)
     print "Delivering "+ str(request)+ "$ to client:"
     for x in paperValues:
         while request >= x:
             print "give"+ str(x)
-            request-=x
-    return remaining
+            request -= x
     print "**** Done ! ****"
+    return remaining
 
 #test any value here
-balance= 1000
-balance= withdraw(balance,250)
+balance = 1000
+balance = withdraw( balance, 750 )
+print "Remaining balance " + str( balance )
