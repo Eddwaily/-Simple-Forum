bc35fa223ba5fd46f8d18f6605637cae45de3110
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-14 15:44:23 +0200

    you can get remaining balance
    withDraw function is now returning the remaining balance

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,17 +1,21 @@
 
-def withDraw(request):
-    money=500
+def withDraw(money,request):
     paperValues=[100,50,10,5,1]
     assert request>0, "Invalid input"
     assert request<=money, "Balance Not Enough !"
-
+    remaining=money-request
     print "*************"
     print "Delivering "+str(request)+"$ to client:"
     for x in paperValues:
         while (request>=x):
             print "give"+str(x)
             request-=x
+    return remaining
     print "Done !"
 
 #test any value here
-withDraw(8)
+balance=500
+balance=withDraw(balance,200)
+balance=withDraw(balance,119)
+balance=withDraw(balance,181)
+balance=withDraw(balance,1)
