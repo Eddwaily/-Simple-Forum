10e32a3342055218f6ea79edfd94687b911775bb
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-15 01:40:35 +0200

    making the code LINEAR

--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -2,22 +2,24 @@
 def withdraw( money, request):
     paperValues= [ 100, 50, 10, 5, 1]
     remaining= money - request
-    
-    if request <= 0 :
-        print "Invalid input !" ; return money
-    if request > money :
-        print "Balance Not Enough !" ; return money
 
-    print "Current Balance: "+ str(money)
-    print "Delivering "+ str(request)+ "$ to client:"
-    for x in paperValues:
-        while request >= x:
-            print "give"+ str(x)
-            request -= x
-    print "**** Done ! ****"
-    return remaining
+    if request <= 0 :
+        print "Invalid input !" ;
+    elif request > money :
+        print "Balance Not Enough !" ;
+
+    else:
+        print "Current Balance: "+ str(money)
+        print "Delivering "+ str(request)+ "$ to client:"
+        for x in paperValues:
+            while request >= x:
+                print "give"+ str(x)
+                request -= x
+        print "**** Done ! ****"
+        print "Remaining balance " + str( remaining )
+        return remaining
+    return money
 
 #test any value here
 balance = 1000
-balance = withdraw( balance, 750 )
-print "Remaining balance " + str( balance )
+balance = withdraw( balance, 2000 )
