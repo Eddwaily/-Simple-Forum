a90c9d16a28ef570c08e92888ace8db9d3c43c71
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-16 01:59:07 +0200

    nothing new

--- atm solution/atm_class.py
+++ atm solution/atm_class.py
@@ -1,16 +1,17 @@
 def line():
-    return "================================================"
-
+    print "="*30
+    
 class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
 
+
+
     def withdraw(self,request):
         paperValues = [ 100, 50, 10, 5, 1]
-        remaining = self.balance - request
 
-        print line()
+        line()
         print "Welcome to " + self.bank_name +" !"
         print "Current Balance: "+ str(self.balance)
 
@@ -19,15 +20,14 @@ class ATM:
         elif request > self.balance :
             print "Balance Not Enough !" ;
         else:
-
             print "Delivering " + str(request) + "$ to client:"
             for x in paperValues:
                 while request >= x:
                     print "give"+ str(x)
                     request -= x
-            print "Remaining balance " + str( remaining )
-            print line()
-            return remaining
+            self.balance -= request
+            print "Remaining balance " + str( self.balance )
+            line()
         return self.balance
 
 #making objects of type ATM
@@ -36,4 +36,3 @@ atm2 = ATM( 1500, "WAHDA BANK")
 
 #testing withdraw method for those objects
 atm1.balance = atm1.withdraw(501)
-atm2.balance = atm2.withdraw(750)
