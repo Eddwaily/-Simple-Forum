6e2f892982d3581dc49442033c896b3885ac57e7
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-17 01:15:46 +0200

    Added a list for withdrawals and date, also added a function to list all the withdrawal for a given account

--- atm solution/atm_class.py
+++ atm solution/atm_class.py
@@ -1,10 +1,16 @@
+from time import ctime, sleep
 class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
+        self.withdrawal_list = []
 
     def _line(self):
         print "="*30
+
+    def _show_withdrawals(self):
+        for withdrawal, date in self.withdrawal_list:
+            print str( withdrawal ) + "$ withdrawn at " + date
 
     def withdraw(self,request):
         paperValues = [ 100, 50, 10, 5, 1]
@@ -18,19 +24,23 @@ class ATM:
         elif request > self.balance :
             print "Balance Not Enough !" ;
         else:
+            self.withdrawal_list.append( [ request, ctime() ] )
+            self.balance -= request
             print "Delivering " + str(request) + "$ to client:"
             for x in paperValues:
                 while request >= x:
                     print "give"+ str(x)
                     request -= x
-            self.balance -= request
             print "Remaining balance " + str( self.balance )
         self._line()
         return self.balance
 
 #making objects of type ATM
 atm1 = ATM( 500, "AMAN BANK")
-atm2 = ATM( 1500, "WAHDA BANK")
 
 #testing withdraw method for those objects
-atm1.balance = atm1.withdraw(501)
+atm1.balance = atm1.withdraw(50)
+sleep(5)
+atm1.balance = atm1.withdraw(150)
+
+atm1._show_withdrawals()
