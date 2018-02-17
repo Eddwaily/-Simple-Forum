081f24a440fa7d1e78ba839d4643e25c66fa853d
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-17 02:00:38 +0200

    using namedtuples to store value,date.

--- atm solution/atm_class.py
+++ atm solution/atm_class.py
@@ -1,19 +1,22 @@
 from time import ctime, sleep
+from collections import namedtuple
+
 class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
+        self.withdrawal_tuple = namedtuple( 'withdrawal','value date' )
         self.withdrawal_list = []
 
     def _line(self):
         print "="*30
 
     def _show_withdrawals(self):
-        for withdrawal, date in self.withdrawal_list:
-            print str( withdrawal ) + "$ withdrawn at " + date
+        for record in self.withdrawal_list:
+            print str( record.value ) + "$ withdrawn at " + record.date
 
     def withdraw(self,request):
-        paperValues = [ 100, 50, 10, 5, 1]
+        paper_values = [ 100, 50, 10, 5, 1]
 
         self._line()
         print "Welcome to " + self.bank_name +" !"
@@ -24,12 +27,12 @@ class ATM:
         elif request > self.balance :
             print "Balance Not Enough !" ;
         else:
-            self.withdrawal_list.append( [ request, ctime() ] )
+            self.withdrawal_list.append( self.withdrawal_tuple( request, ctime() ) )
             self.balance -= request
             print "Delivering " + str(request) + "$ to client:"
-            for x in paperValues:
+            for x in paper_values:
                 while request >= x:
-                    print "give"+ str(x)
+                    print "give" + str(x)
                     request -= x
             print "Remaining balance " + str( self.balance )
         self._line()
@@ -42,5 +45,4 @@ atm1 = ATM( 500, "AMAN BANK")
 atm1.balance = atm1.withdraw(50)
 sleep(5)
 atm1.balance = atm1.withdraw(150)
-
 atm1._show_withdrawals()
