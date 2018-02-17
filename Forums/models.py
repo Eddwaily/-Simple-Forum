4b35217558ecfe769781fd2df79cd79edb260338
Mahmoud Eddwaily <dwaily@yahoo.com>
2018-02-17 03:47:05 +0200

    nothing new

--- Forums/models.py
+++ Forums/models.py
@@ -1,6 +1,5 @@
 class member:
     member_count = 0
-
     def __init__( self, name, age):
         self.name = name
         self.age = age
@@ -8,9 +7,8 @@ class member:
 
 
 class post:
-    post_count = 0
-
+    posts_count = 0
     def __init__( self, title, content):
         self.title = title
         self.content = content
-        post.post_count += 1
+        post.posts_count += 1
